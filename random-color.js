6efb7bf8b3362c69a9d426be7bb4598dea4fefb6
Matt Cook <32623301+sciencefidelity@users.noreply.github.com>
2021-02-27 21:38:54 +0000

    Add files via upload

---
+++ random-color.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let r = 0;
+let g = 0;
+let b = 0;
+
+document.getElementById("main").addEventListener("click", colorFunction);
+
+function colorFunction() {
+	document.getElementById("main").style.background = "rgb(" + myFun(r) + "," + myFun(g) + "," + myFun(b) + ")";
+	document.getElementById("colorRGB").innerHTML = myFun(r) + ", " + myFun(g) + ", " + myFun(b);
+}
+
+function myFun(r, g, b) {
+	return r = (Math.random() * 255).toFixed(0);
+	return g = (Math.random() * 255).toFixed(0);
+	return b = (Math.random() * 255).toFixed(0);
+}
