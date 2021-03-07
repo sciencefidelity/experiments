5991f998865274baa1457b2be468965832d35406
Matt Cook <32623301+sciencefidelity@users.noreply.github.com>
2021-03-07 21:12:24 +0000

    Add files via upload

--- random-color/js/app.js
+++ random-color/js/app.js
@@ -7,12 +7,12 @@ let b = 0;
 document.getElementById("main").addEventListener("click", colorFunction);
 
 function colorFunction() {
-	document.getElementById("main").style.background = "rgb(" + myFun(r) + "," + myFun(g) + "," + myFun(b) + ")";
-	document.getElementById("colorRGB").innerHTML = "rgb(" + myFun(r) + ", " + myFun(g) + ", " + myFun(b) + ")";
+  document.getElementById("main").style.background = "rgb(" + myFun(r) + "," + myFun(g) + "," + myFun(b) + ")";
+  document.getElementById("colorRGB").innerHTML = "rgb(" + myFun(r) + ", " + myFun(g) + ", " + myFun(b) + ")";
 }
 
 function myFun(r, g, b) {
-	return r = (Math.random() * 255).toFixed(0);
-	return g = (Math.random() * 255).toFixed(0);
-	return b = (Math.random() * 255).toFixed(0);
+  return r = (Math.random() * 255).toFixed(0);
+  return g = (Math.random() * 255).toFixed(0);
+  return b = (Math.random() * 255).toFixed(0);
 }
