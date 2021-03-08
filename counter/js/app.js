62115d224bb5a2f0aed37240973d84115a94704c
Matt Cook <32623301+sciencefidelity@users.noreply.github.com>
2021-03-08 22:44:02 +0000

    Add files via upload

---
+++ counter/js/app.js
@@ -0,0 +1,22 @@
+// Count-up that triggers on load or refresh
+// adjust speed and updateCount to change duration
+
+const counters = document.querySelectorAll('.counter');
+const speed = 200;
+
+counters.forEach(counter => {
+  const updateCount = () => {
+    const target = +counter.getAttribute('data-target');
+    const count = +counter.innerText;
+    
+    const inc = target / speed;
+  
+    if(count < target) {
+      counter.innerText = Math.ceil(count + inc);
+      setTimeout(updateCount, 10);
+    } else {
+      count.innerText = target;
+    }
+  }
+  updateCount();
+});
