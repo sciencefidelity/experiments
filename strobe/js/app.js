6587f991c7db112b51372a927a4aac3555b37f89
Matt Cook <32623301+sciencefidelity@users.noreply.github.com>
2021-03-08 23:13:17 +0000

    Add files via upload

---
+++ strobe/js/app.js
@@ -0,0 +1,15 @@
+// Animated colour changer / strobe / disco light.
+
+"use strict"
+
+setInterval(colorChange, 2000)
+let shape = document.getElementById("shape")
+let i = 0
+shape.className = "c" + i
+function colorChange() {
+  i++
+  if (i === 6) {
+    i = 0
+  }
+  shape.className = "c" + i
+}
