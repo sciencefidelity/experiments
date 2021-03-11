0dbac9773aa97f66d82625ebf703693c942d1b0d
Matt Cook <32623301+sciencefidelity@users.noreply.github.com>
2021-03-11 00:00:24 +0000

    Update app.js

--- counter/js/app.js
+++ counter/js/app.js
@@ -1,22 +1,22 @@
 // Count-up that triggers on load or refresh
 // adjust speed and updateCount to change duration
 
-const counters = document.querySelectorAll('.counter');
-const speed = 200;
+const counters = document.querySelectorAll('.counter')
+const speed = 200
 
 counters.forEach(counter => {
   const updateCount = () => {
-    const target = +counter.getAttribute('data-target');
-    const count = +counter.innerText;
+    const target = +counter.getAttribute('data-target')
+    const count = +counter.innerText
     
-    const inc = target / speed;
+    const inc = target / speed
   
     if(count < target) {
-      counter.innerText = Math.ceil(count + inc);
-      setTimeout(updateCount, 10);
+      counter.innerText = Math.ceil(count + inc)
+      setTimeout(updateCount, 10)
     } else {
-      count.innerText = target;
+      count.innerText = target
     }
   }
-  updateCount();
-});
+  updateCount()
+})
