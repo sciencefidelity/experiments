b89412b2e2a92de346ed10a2fae6c300f7841f56
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-03-13 22:38:10 +0000

    Add a function to download notes to a markdown file.

--- notepad/js/app.js
+++ notepad/js/app.js
@@ -1,5 +1,7 @@
 "use strict"
 
+// Switch between "dark mode" and "light mode".
+
 const modeSwitch = document.getElementById("modeSwitch")
 const textLeft = document.getElementById("textLeft")
 const textRight = document.getElementById("textRight")
@@ -13,6 +15,25 @@ modeSwitch.onclick = () => {
     textRight.className = "light-mode-right"
   }
 }
+
+// Download notes to a file.
+
+const downloadToFile = (content, filename, contentType) => {
+  const a = document.createElement("a");
+  const file = new Blob([content], {type: contentType});
+  
+  a.href= URL.createObjectURL(file);
+  a.download = filename;
+  a.click();
+
+  URL.revokeObjectURL(a.href);
+};
+
+document.getElementById("save").addEventListener("click", () => {
+  const textArea = document.getElementById("textLeft");
+  
+  downloadToFile(textArea.value, "browser-note.md", "text/markdown");
+});
 
 
 
