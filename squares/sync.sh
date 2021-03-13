300c73b8b24c95cab422318177f46a7b0878a59a
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-03-13 19:59:03 +0000

    Add favicon to prevent console error

--- squares/sync.sh
+++
@@ -1,6 +0,0 @@
-#! bin/zsh
-
-# A simple script for automating scss and browser-sync.
-
-browser-sync start --server --no-open --files "./css/*.css" "*.html" "./js/*.js" &
-sass --watch ./scss/style.scss ./css/style.css
