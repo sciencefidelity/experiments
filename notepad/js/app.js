562858a1260a50072c29eac2af6f14db7cb84677
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-03-13 22:04:03 +0000

    Set up mode switching

--- notepad/js/app.js
+++ notepad/js/app.js
@@ -1,3 +1,20 @@
 "use strict"
 
-console.log('Hello world!')
+// console.log('Hello world!')
+
+const modeSwitch = document.getElementById("modeSwitch")
+const textLeft = document.getElementById("textLeft")
+const textRight = document.getElementById("textRight")
+
+modeSwitch.onclick = () => { 
+  if (textLeft.className === "light-mode-left") {
+    textLeft.className = "dark-mode-left"
+    textRight.className = "dark-mode-right"
+  } else {
+    textLeft.className = "light-mode-left"
+    textRight.className = "light-mode-right"
+  }
+}
+
+
+
