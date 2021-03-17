fe449f52c1ade6b3325ebe8cd885f97d14312318
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-03-17 22:05:28 +0000

    Change file structure

---
+++ counter/version-2/js/app.js
@@ -0,0 +1,29 @@
+// Count-up that triggers on click of a button
+// adjust speed and updateCount to change duration
+
+"use strict"
+
+const counters = document.querySelectorAll('.counter')
+const btn = document.getElementById('btnContainer')
+const speed = 200
+
+document..onclick = () => {
+  console.log('clicked')
+  counters.forEach(counter => {
+    counter.innerText = 0
+    const updateCount = () => {
+      const target = +counter.getAttribute('data-target')
+      const count = +counter.innerText
+      
+      const inc = target / speed
+    
+      if(count < target) {
+        counter.innerText = Math.ceil(count + inc)
+        setTimeout(updateCount, 10)
+      } else {
+        count.innerText = target
+      }
+    }
+    updateCount()
+  })
+}
