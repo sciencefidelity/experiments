643b0ffb7d060c3daa2b40b2b543a4a84d156c66
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-03-17 23:04:19 +0000

    Move onclick from html into js

--- counter/version-2/js/app.js
+++ counter/version-2/js/app.js
@@ -4,17 +4,17 @@
 "use strict"
 
 const animationDuration = 2000
-const frameDuration = 1000 / 60
+const frameDuration = 1000 / 20
 const totalFrames = Math.round(animationDuration / frameDuration)
 const easeOutQuad = t => t * (2 - t)
 
 const animateCountUp = el => {
   let frame = 0
-  const countTo = parseInt(el.getAttribute('data-target'), 10)
+  const target = parseInt(el.getAttribute('data-target'), 10)
   const counter = setInterval(() => {
     frame++
-    const progress = easeOutQuad( frame / totalFrames)
-    const currentCount = Math.round(countTo * progress)
+    const progress = easeOutQuad(frame / totalFrames)
+    const currentCount = Math.round(target * progress)
 
     if (parseInt(el.innerHTML, 10) !== currentCount) {
       el.innerHTML = currentCount
@@ -26,7 +26,9 @@ const animateCountUp = el => {
   }, frameDuration)
 }
 
-const startCount = () => {
-  const countupEls = document.querySelectorAll('.counter')
-  countupEls.forEach(animateCountUp)
+const btn = document.getElementById('btn')
+
+btn.onclick = () => {
+  const counters = document.querySelectorAll('.counter')
+  counters.forEach(animateCountUp)
 }
