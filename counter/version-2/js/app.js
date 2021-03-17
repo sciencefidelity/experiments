a3667b1c9ec403723c3a0239d33c0ee2bdd17d8c
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-03-17 22:43:06 +0000

    Use data-target as count target

--- counter/version-2/js/app.js
+++ counter/version-2/js/app.js
@@ -5,28 +5,28 @@
 
 const animationDuration = 2000
 const frameDuration = 1000 / 60
-const totalFrames = Math.round( animationDuration / frameDuration )
-const easeOutQuad = t => t * ( 2 - t )
+const totalFrames = Math.round(animationDuration / frameDuration)
+const easeOutQuad = t => t * (2 - t)
 
 const animateCountUp = el => {
-  let frame = 0;
-  const countTo = parseInt( el.innerHTML, 10 )
-  const counter = setInterval( () => {
+  let frame = 0
+  const countTo = parseInt(el.getAttribute('data-target'), 10)
+  const counter = setInterval(() => {
     frame++
-    const progress = easeOutQuad( frame / totalFrames )
-    const currentCount = Math.round( countTo * progress )
+    const progress = easeOutQuad( frame / totalFrames)
+    const currentCount = Math.round(countTo * progress)
 
-    if ( parseInt( el.innerHTML, 10 ) !== currentCount ) {
+    if (parseInt(el.innerHTML, 10) !== currentCount) {
       el.innerHTML = currentCount
     }
 
-    if ( frame === totalFrames ) {
-      clearInterval( counter )
+    if (frame === totalFrames) {
+      clearInterval(counter)
     }
-  }, frameDuration )
+  }, frameDuration)
 }
 
-const runAnimations = () => {
-  const countupEls = document.querySelectorAll( '.counter' )
-  countupEls.forEach( animateCountUp )
+const startCount = () => {
+  const countupEls = document.querySelectorAll('.counter')
+  countupEls.forEach(animateCountUp)
 }
