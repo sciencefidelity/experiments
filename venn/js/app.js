acb43049a55c2c630ab73bf66b6915d55c67bdec
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-03-19 23:47:18 +0000

    Make the switch change color

--- venn/js/app.js
+++ venn/js/app.js
@@ -1,14 +1,17 @@
 "use strict"
 
-console.log("hello")
+console.log("Venn Diagram")
 
 const modeSwitch = document.getElementById("mode")
 const background = document.getElementById("site-main")
+const mode = document.getElementById("mode")
 
 modeSwitch.onclick = () => {
   if (background.className === "light") {
     background.className = "dark"
+    mode.className = "darkmode"
   } else {
     background.className = "light"
+    mode.className = "lightmode"
   }
 }
