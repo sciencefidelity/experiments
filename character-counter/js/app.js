cde9b679ffb9d6c51b5e42e40c3fcb207a440bdc
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-03-23 23:27:47 +0000

    Improve function

--- character-counter/js/app.js
+++ character-counter/js/app.js
@@ -3,15 +3,6 @@
 const input = document.querySelector(".input")
 const output = document.querySelector(".output")
 
-// const updateCount = (str) => {
-//   let lng = str.length
-//   if (lng === 1) {
-//     output.innerHTML = lng + " character"
-//     return
-//   }
-//   output.innerHTML = lng + " characters"
-// }
-
 input.addEventListener('input', function(e) {
   const target = e.target
   const lgn = target.value.length
