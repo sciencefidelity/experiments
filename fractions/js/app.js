e22d921b5484bc33c255f2cf9bdddc1b08aa1ce9
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-03-26 00:31:20 +0000

    Start/stop button added

--- fractions/js/app.js
+++ fractions/js/app.js
@@ -1,3 +1,15 @@
 "use strict"
 
-console.log("Turntables")
+const btn = document.getElementById("btn")
+const turntable = document.getElementById("container")
+
+btn.onclick = () => {
+  console.log("Clicked")
+  if (turntable.className === "stop") {
+    turntable.className = "spin"
+  } else {
+    turntable.className = "stop"
+  }
+}
+
+
