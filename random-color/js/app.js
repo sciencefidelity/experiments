e3fc8477a9031d184a1e8b13054034d8a1a13537
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-03-31 23:03:00 +0000

    Fix console error

--- random-color/js/app.js
+++ random-color/js/app.js
@@ -5,17 +5,20 @@
 
 const canvas = document.querySelector("main")
 const code = document.querySelector("p")
-let r = 0
-let g = 0
-let b = 0
+const r = 0
+const g = 0
+const b = 0
 
 canvas.onclick = () => {
-  canvas.style.background = "rgb(" + color(r) + "," + color(g) + "," + color(b) + ")"
-  code.innerHTML = "rgb(" + color(r) + ", " + color(g) + ", " + color(b) + ")"
+  canvas.style.background = 
+    "rgb(" + color(r) + "," + color(g) + "," + color(b) + ")"
+  code.innerHTML = 
+    "rgb(" + color(r) + ", " + color(g) + ", " + color(b) + ")"
 }
 
-function color(r, g, b) {
-  return r = (Math.random() * 255).toFixed(0)
-  return g = (Math.random() * 255).toFixed(0)
-  return b = (Math.random() * 255).toFixed(0)
+const color = (r, g, b) => {
+  r = (Math.random() * 255).toFixed(0)
+  g = (Math.random() * 255).toFixed(0)
+  b = (Math.random() * 255).toFixed(0)
+  return r, g, b
 }
