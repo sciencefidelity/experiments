6b96396abdbf44eceb13456eaaa6692fa66e1de6
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-24 17:14:09 +0000

    Add two maps

--- maps/js/app.js
+++ maps/js/app.js
@@ -2,3 +2,16 @@
 
 "use strict"
 
+let map1
+let map2
+
+function initMap() {
+  map1 = new google.maps.Map(document.getElementById("map1"), {
+    center: { lat: 51.478, lng: -0.002 },
+    zoom: 15,
+  })
+  map2 = new google.maps.Map(document.getElementById("map2"), {
+    center: { lat: 0, lng: -0.002 },
+    zoom: 15,
+  })
+}
