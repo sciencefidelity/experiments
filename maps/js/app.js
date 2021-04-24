a52ebf4631c9b9a215ba69207b0b2b5e642fac7e
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-24 23:00:22 +0000

    Randomize coordinates of 8 maps

--- maps/js/app.js
+++ maps/js/app.js
@@ -1,76 +1,68 @@
+
 // Random map generator
 
 "use strict"
 
-// const googleScript = document.createElement('script')
-// googleScript.src = 'https://maps.googleapis.com/maps/api/js?key=GOOGLE_API_KEY&callback=initMap'
-// googleScript.async = true
+const googleScript = document.createElement('script')
+googleScript.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDhftVfSKZHAeEjN--y_7vh2t5BLhpT3JA&callback=initMap'
+googleScript.async = true
+
+const zoom = 10
+let lat = []
+let lgn = []
+
+// Random number generator
+
+function rand(min, max){
+  return (Math.pow(10,14)*Math.random()
+  *Math.random())%(max-min+1)+min
+}
+
+for (let i = 0; i < 8; i++) {
+  lat.push(Number(rand(-84, 84).toFixed(3)))
+}
+
+for (let i = 0; i < 8; i++) {
+  lgn.push(Number(rand(-180, 180).toFixed(3)))
+}
 
 // Google Maps Platform
 
-// let map1
-// let map2
-// let map3
-// let map4
-// let map5
-// let map6
-// let map7
-// let map8
-// let map9
+let map1, map2, map3, map4, map5, map6, map7, map8, map9
 // 
-// function initMap() {
-//   map1 = new google.maps.Map(document.getElementById("map1"), {
-//     center: { lat: 51.478, lng: -0.002 },
-//     zoom: 8,
-//   })
-//   map2 = new google.maps.Map(document.getElementById("map2"), {
-//     center: { lat: 50, lng: -50 },
-//     zoom: 8,
-//   })
-//   map3 = new google.maps.Map(document.getElementById("map3"), {
-//     center: { lat: 25, lng: -25 },
-//     zoom: 8,
-//   })
-//   map4 = new google.maps.Map(document.getElementById("map4"), {
-//     center: { lat: 0, lng: -0.002 },
-//     zoom: 8,
-//   })
-//   map5 = new google.maps.Map(document.getElementById("map5"), {
-//     center: { lat: 23, lng: -20 },
-//     zoom: 8,
-//   })
-//   map6 = new google.maps.Map(document.getElementById("map6"), {
-//     center: { lat: 32, lng: 10 },
-//     zoom: 8,
-//   })
-//   map7 = new google.maps.Map(document.getElementById("map7"), {
-//     center: { lat: 16, lng: -65 },
-//     zoom: 8,
-//   })
-//   map8 = new google.maps.Map(document.getElementById("map8"), {
-//     center: { lat: -16, lng: 65 },
-//     zoom: 8,
-//   })
-// }
-// 
-// document.head.appendChild(googleScript)
+function initMap() {
+  map1 = new google.maps.Map(document.getElementById("map1"), {
+    center: { lat: lat[0], lng: lgn[0] },
+    zoom: zoom,
+  })
+  map2 = new google.maps.Map(document.getElementById("map2"), {
+    center: { lat: lat[1], lng: lgn[1] },
+    zoom: zoom,
+  })
+  map3 = new google.maps.Map(document.getElementById("map3"), {
+    center: { lat: lat[2], lng: lgn[2] },
+    zoom: zoom,
+  })
+  map4 = new google.maps.Map(document.getElementById("map4"), {
+    center: { lat: lat[3], lng: lgn[3] },
+    zoom: zoom,
+  })
+  map5 = new google.maps.Map(document.getElementById("map5"), {
+    center: { lat: lat[4], lng: lgn[4] },
+    zoom: zoom,
+  })
+  map6 = new google.maps.Map(document.getElementById("map6"), {
+    center: { lat: lat[5], lng: lgn[5] },
+    zoom: zoom,
+  })
+  map7 = new google.maps.Map(document.getElementById("map7"), {
+    center: { lat: lat[6], lng: lgn[6] },
+    zoom: zoom,
+  })
+  map8 = new google.maps.Map(document.getElementById("map8"), {
+    center: { lat: lat[7], lng: lgn[7] },
+    zoom: zoom,
+  })
+}
 
-// Apple Maps
-
-mapkit.init({
-  authorizationCallback: function(done) {
-      var xhr = new XMLHttpRequest();
-      xhr.open("GET", "/services/jwt");
-      xhr.addEventListener("load", function() {
-          done(this.responseText);
-      });
-      xhr.send();
-  }
-});
-
-let Cupertino = new mapkit.CoordinateRegion(
-  new mapkit.Coordinate(37.3316850890998, -122.030067374026),
-  new mapkit.CoordinateSpan(0.167647972, 0.354985255)
-);
-let map = new mapkit.Map("map1")
-map.region = Cupertino
+document.head.appendChild(googleScript)
