3f53c33d708a5f9bfa909d24bfcf01feadfd70e0
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-25 15:55:27 +0000

    Use Snowpack

---
+++ maps/snowpack.config.js
@@ -0,0 +1,27 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    /* ... */
+  },
+  root: './src/',
+  plugins: [
+    [
+      '@snowpack/plugin-sass',
+      {
+        'native': true
+      },
+    ],
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
