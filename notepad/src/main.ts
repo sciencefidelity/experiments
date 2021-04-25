9c543d1dff48724270c050aef3e3931ad1c5bd52
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-25 23:52:47 +0000

    Convert to typescript and fix types

--- notepad/src/main.ts
+++ notepad/src/main.ts
@@ -1,19 +1,21 @@
 
-"use strict"
+'use strict'
 
-const modeSwitch = document.querySelector('#modeSwitch')
-const textLeft = document.querySelector('#textLeft')
-const textRight = document.querySelector('#textRight')
+const modeSwitch = document.querySelector<HTMLDivElement>('#modeSwitch')!
+const textLeft = document.querySelector<HTMLDivElement>('#textLeft')!
+const textRight = document.querySelector<HTMLDivElement>('#textRight')!
 
 // initialise the input content
-textLeft.textContent = ""
+textLeft.textContent = `&nbsp;`
 
 // save input text into local storage
-textLeft.addEventListener('input', updateValue)
+textLeft?.addEventListener('input', updateValue)
 
-function updateValue(e) {
-  localStorage.setItem('leftText', textLeft.textContent)
-  textRight.textContent = e.target.textContent
+function updateValue(e: any) {
+  if (textLeft !== null) {
+    localStorage.setItem('leftText', (textLeft.textContent) || '')
+    textRight.textContent = (<HTMLDivElement>e.target).textContent
+  }
 }
 
 textLeft.textContent = localStorage.getItem('leftText')
@@ -35,9 +37,9 @@ modeSwitch.onclick = () => {
 
 // Download notes to a markdown file.
 
-const save = document.querySelector('#save')
+const save = document.querySelector<HTMLDivElement>('#save')!
 
-const fileDownload = (content, filename, contentType) => {
+const fileDownload = (content: string, filename: string, contentType: string) => {
   const a = document.createElement('a')
   const file = new Blob([content], {type: contentType})
 
@@ -47,7 +49,6 @@ const fileDownload = (content, filename, contentType) => {
   URL.revokeObjectURL(a.href)
 }
 
-textLeft.onclick = () => {
-  fileDownload(text.textContent, "browser-note.md", "text/markdown")
+save.onclick = () => {
+  fileDownload((textLeft.textContent) || '', "browser-note.md", "text/markdown")
 }
-
