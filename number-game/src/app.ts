3caa7e78e74a19b583af33848d5f9414715f3f8e
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-05-22 15:37:48 +0000

    Set up documents and variables

---
+++ number-game/src/app.ts
@@ -0,0 +1,14 @@
+'use strict'
+
+const randomNumber = Math.floor(Math.random() * 100) + 1
+
+const guesses = document.querySelector('.guesses')
+const lastResult = document.querySelector('.lastResult')
+const lowOrHigh = document.querySelector('.lowOrHIgh')
+
+const guessSubmit = document.querySelector('.guessSubmit')
+const guessField = document.querySelector('guessField')
+
+let guessCount = 1
+let resetButton
+
