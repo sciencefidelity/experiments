2a98fd0eb1013e8479e4e885e917dba5c3a1ae1b
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-05-22 16:19:10 +0000

    Create functions to set the game to over and reset the game

--- number-game/src/app.ts
+++ number-game/src/app.ts
@@ -13,6 +13,64 @@ let guessCount = 1
 let resetButton
 
 function checkGuess() {
-  alert('I\'m a placeholder')
+  let userGuess = Number(guessField.value)
+  if (guessCount === 1) {
+    guesses.textContent = 'Previous guesses: '
+  }
+  guesses.textContent += userGuess + ' '
+
+  if (userGuesses === randomNumber) {
+    lastResult.textContent = 'Congratulations! You got it right!'
+    lastResult.style.backgroundColor = "green"
+    lowOrHigh?.textContent = ''
+    setGameOver()
+  } else if (guessCount === 10) {
+    lastResult?.textContent = '!!!GAME OVER!!!'
+    setGameOver()
+  } else {
+    lastResult.textContent = 'Wrong!'
+    lastResult.style.backgroundColor = 'red'
+    if(userGuess < randomNumber) {
+      lowOrHigh.textContent = 'Last guess was too low!'
+    } else if(userGuess > randomNumber) {
+      lowOrHigh.textContent = 'Last guess was too high!'
+    }
+  }
+
+  guessCount++
+  guessField.value = ''
+  guessField.focus()
 }
+
+guessSubmit?.addEventListener('click', checkGuess)
+
+function setGameOver() {
+  guessField.disabled = true
+  guessField.disabled = true
+  resetButton = document.createElement('button')
+  resetButton.textContent = 'Start new game'
+  document.body.append(resetButton)
+  resetButton.addEventListener('click', resetGame)
+}
+
+function resetGame() {
+  guessCount = 1
+
+  const resetParas = document.querySelectorAll('.resultParas p')
+  for (let i = 0 ; i < resetParas.length ; i++) {
+    resetParas[i].textContent = ''
+  }
+
+  resetButton.parentNode.removeChild(resetButton)
+
+  guessField.disabled = false
+  guessSubmit.disabled = false
+  guessField.value = ''
+  guessField.focus()
+
+  lastResult.style.backgroundColor = 'white'
+
+  randomNumber = Math.floor(Math.random() * 100) + 1
+}
+
 
