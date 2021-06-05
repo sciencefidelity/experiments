57c89c04a04d706ce388041c542a958e234f2490
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-05 23:20:24 +0000

    Fix bugs

--- scroll-video/src/app.ts
+++ scroll-video/src/app.ts
@@ -2,15 +2,18 @@
 
 let frameNumber = 0
 const playbackConst = 500
-const setHeight = document.querySelector<HTMLDivElement>('set-height')
+const setHeight = document.querySelector<HTMLDivElement>('#set-height')
 const vid = document.querySelector<HTMLVideoElement>('#v0')
 
 vid!.addEventListener('loadedmetadata', function() {
   setHeight!.style.height = Math.floor(vid!.duration) * playbackConst + 'px'
 })
 
-const scrollPlay = () => {
-  let frameNumber = window.pageYOffset / playbackConst
+function scrollPlay() {
+  frameNumber = window.pageYOffset / playbackConst
   vid!.currentTime = frameNumber
   window.requestAnimationFrame(scrollPlay)
+  console.log(frameNumber)
 }
+
+window.requestAnimationFrame(scrollPlay)
