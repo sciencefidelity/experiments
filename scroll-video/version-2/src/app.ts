ea4fbddf9fde71c04a6f196745023464343ffd3e
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-07 23:23:21 +0000

    New version using images rather than video

--- scroll-video/version-2/src/app.ts
+++ scroll-video/version-2/src/app.ts
@@ -1,19 +1,44 @@
 'use strict'
 
-let frameNumber = 0
-const playbackConst = 500
-const setHeight = document.querySelector<HTMLDivElement>('#set-height')
-const vid = document.querySelector<HTMLVideoElement>('#v0')
+const html = document.documentElement
+const canvas = document.querySelector('#coin')
+const context = canvas.getContext('2d')
 
-vid!.addEventListener('loadedmetadata', function() {
-  setHeight!.style.height = Math.floor(vid!.duration) * playbackConst + 'px'
-})
+const frameCount = 602
+const currentFrame = index => (
+  `../videos/coin/frame-${index.toString().padStart(5, '0')}.jpg.webp`
+)
 
-function scrollPlay() {
-  frameNumber = window.pageYOffset / playbackConst
-  vid!.currentTime = frameNumber
-  window.requestAnimationFrame(scrollPlay)
-  console.log(frameNumber)
+const preloadImages = () => {
+  for (let i = 1; i < frameCount; i++) {
+    const img = new Image()
+    img.src = currentFrame(i)
+  }
 }
 
-window.requestAnimationFrame(scrollPlay)
+const img = new Image()
+img.src = currentFrame(1)
+canvas.width=1920
+canvas.height=1080
+img.onload=function(){
+  context.drawImage(img, 0, 0)
+}
+
+const updateImage = index => {
+  img.src = currentFrame(index)
+  context.drawImage(img, 0, 0)
+}
+
+window.addEventListener('scroll', () => {
+  const scrollTop = html.scrollTop
+  const maxScrollTop = html.scrollHeight - window.innerHeight
+  const scrollFraction = scrollTop / maxScrollTop
+  frameIndex = Math.min(
+    frameCount - 1,
+    Math.ceil(scrollFraction * frameCount)
+  )
+
+  requestAnimationFrame(() => updateImage(frameIndex + 1))
+})
+
+preloadImages()
