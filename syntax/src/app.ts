dfc618145d6ae78cd4873bbdfc3ae1beaf1fa3e9
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-16 23:39:48 +0000

    Add strong typing for event object

--- syntax/src/app.ts
+++ syntax/src/app.ts
@@ -5,15 +5,19 @@ const loadContent = localStorage.getItem('content')
 const input: HTMLPreElement = document.querySelector('#input') as HTMLPreElement
 const output: HTMLDivElement = document.querySelector('#output') as HTMLDivElement
 
+interface Event {
+  readonly target: EventTarget | null
+}
+
 // initialise the input content
 input.innerHTML = ''
 
 // save input text into local storage
 input.addEventListener('input', updateValue)
 
-function updateValue(e: any) {
+function updateValue(e: Event) {
   localStorage.setItem('content', input.innerHTML)
-  output.innerHTML = e.target.textContent
+  output.innerHTML = (e.target as HTMLDivElement).textContent!
     .replaceAll('Matt', '<span style="color:orange">Matt</span>')
     .replaceAll('World', '<span style="color:limegreen">World</span>')
 }
