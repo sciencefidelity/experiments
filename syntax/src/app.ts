a0c6b5f28e44131f78a2c29c23d69b21775b5a5e
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-16 23:56:17 +0000

    Cahnge the color or a word dynamically

--- syntax/src/app.ts
+++ syntax/src/app.ts
@@ -1,22 +1,23 @@
 
-"use strict"
+'use strict'
 
 const input: Element = document.querySelector('#input')
 const output: Element = document.querySelector('#output')
 
 // initialise the input content
-input.textContent = ""
+input.textContent = ''
 
 // save input text into local storage
 input.addEventListener('input', updateValue)
 
-function updateValue(e: any) {
+function updateValue(e) {
   localStorage.setItem('content', input.textContent)
-  output.textContent = e.target.textContent
+  output.innerHTML = e.target.textContent.replace('Matt', '<span style="color:orange">Matt</span>')
 }
 
 // copy input text into output
 let re = /\**/gi
 input.textContent = localStorage.getItem('content')
 const p: string = localStorage.getItem('content')
-output.textContent = p.replace(re, '')
+output.innerHTML = p
+
