2ae7b72fa79a18c798d551f330b438b5e7df5190
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-02 01:26:15 +0000

    Fix linting warnings

--- scroll-video/version-1/src/app.ts
+++ scroll-video/version-1/src/app.ts
@@ -2,16 +2,16 @@
 
 let frameNumber = 0
 const playbackConst = 500
-const setHeight = document.querySelector<HTMLDivElement>("#set-height")
-const vid = document.querySelector<HTMLVideoElement>("#v0")
+const setHeight = document.querySelector("#set-height") as HTMLDivElement
+const vid = document.querySelector("#v0") as HTMLVideoElement
 
-vid!.addEventListener("loadedmetadata", function () {
-  setHeight!.style.height = Math.floor(vid!.duration) * playbackConst + "px"
+vid.addEventListener("loadedmetadata", function () {
+  setHeight.style.height = Math.floor(vid.duration) * playbackConst + "px"
 })
 
 function scrollPlay() {
   frameNumber = window.pageYOffset / playbackConst
-  vid!.currentTime = frameNumber
+  vid.currentTime = frameNumber
   window.requestAnimationFrame(scrollPlay)
   console.log(frameNumber)
 }
--- scroll-video/version-2/src/coin.ts
+++ scroll-video/version-2/src/coin.ts
@@ -1,6 +1,6 @@
 const html = document.documentElement
-const canvas = <HTMLCanvasElement>document.getElementById("coin")
-const context = canvas!.getContext("2d")
+const canvas = document.getElementById("coin") as HTMLCanvasElement
+const context = canvas.getContext("2d")
 
 const frameCount = 602
 const currentFrame = (index: number) =>
@@ -17,15 +17,15 @@ const preloadImages = () => {
 
 const img = new Image()
 img.src = currentFrame(1)
-canvas!.width = 1920
-canvas!.height = 1080
+canvas.width = 1920
+canvas.height = 1080
 img.onload = function () {
-  context!.drawImage(img, 0, 0)
+  if (context) context.drawImage(img, 0, 0)
 }
 
 const updateImage = (index: number) => {
   img.src = currentFrame(index)
-  context!.drawImage(img, 0, 0)
+  if (context) context.drawImage(img, 0, 0)
 }
 
 window.addEventListener("scroll", () => {
