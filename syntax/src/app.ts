26f96fc40e7f7eb08895cc3206a996fdb5e38b08
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-02 01:02:40 +0000

    Fix linting warnings

--- syntax/src/app.ts
+++ syntax/src/app.ts
@@ -17,20 +17,28 @@ input.innerHTML = ""
 input.addEventListener("input", updateValue)
 
 function updateValue(e: Event) {
+  const target = e.target as HTMLDivElement
   localStorage.setItem("content", input.innerHTML)
-  output.innerHTML = (e.target as HTMLDivElement)
-    .textContent!.replaceAll("Matt", '<span style="color:orange">Matt</span>')
-    .replaceAll("World", '<span style="color:limegreen">World</span>')
+  if (target.textContent)
+    output.innerHTML = target.textContent
+      //eslint-disable-next-line quotes
+      .replaceAll("Matt", '<span style="color:orange">Matt</span>')
+      //eslint-disable-next-line quotes
+      .replaceAll("World", '<span style="color:limegreen">World</span>')
 }
 
 // copy input text into output
 if (loadContent) {
   input.innerHTML = loadContent
+    //eslint-disable-next-line quotes
     .replaceAll("Matt", '<span style="color:orange">Matt</span>')
+    //eslint-disable-next-line quotes
     .replaceAll("World", '<span style="color:limegreen">World</span>')
 }
 if (loadContent) {
   output.innerHTML = loadContent
+    //eslint-disable-next-line quotes
     .replaceAll("Matt", '<span style="color:orange">Matt</span>')
+    //eslint-disable-next-line quotes
     .replaceAll("World", '<span style="color:limegreen">World</span>')
 }
