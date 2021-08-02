540594267ad756556b0fa4ff1c2a57c3916c2616
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-02 01:34:49 +0000

    Fix linting warnings

--- random-color/src/app.ts
+++ random-color/src/app.ts
@@ -3,20 +3,28 @@
 
 "use strict"
 
-const canvas: HTMLElement = document.querySelector("main")
-const code: HTMLParagraphElement = document.querySelector("p")
-let r: number = 0
-let g: number = 0
-let b: number = 0
+const canvas = document.querySelector("main") as HTMLCanvasElement
+const code = document.querySelector("p") as HTMLParagraphElement
+const r = 0
+const g = 0
+const b = 0
 
 canvas.onclick = () => {
-  canvas.style.background = `rgb(${color(r)}, ${color(g)}, ${color(b)})`
-  code.innerHTML = `rgb(${color(r)}, ${color(g)}, ${color(b)})`
+  canvas.style.background = `rgb(${colorR(r)}, ${colorG(g)}, ${colorB(b)})`
+  code.innerHTML = `rgb(${colorR(r)}, ${colorG(g)}, ${colorB(b)})`
 }
 
-const color = (r, g, b) => {
-  r = (Math.random() * 255).toFixed(0)
-  g = (Math.random() * 255).toFixed(0)
-  b = (Math.random() * 255).toFixed(0)
-  return r, g, b
+const colorR = (r: number) => {
+  r = Number((Math.random() * 255).toFixed(0))
+  return r
+}
+
+const colorG = (g: number) => {
+  g = Number((Math.random() * 255).toFixed(0))
+  return g
+}
+
+const colorB = (b: number) => {
+  b = Number((Math.random() * 255).toFixed(0))
+  return b
 }
