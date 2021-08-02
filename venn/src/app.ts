2b0bbcd3a85a86896e4cecee917f3a5c5f66c1d8
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-02 00:01:39 +0000

    Prettify

--- venn/src/app.ts
+++ venn/src/app.ts
@@ -4,12 +4,13 @@ const modeSwitch: HTMLElement | null = document.getElementById("mode")
 const background: HTMLElement | null = document.getElementById("site-main")
 const mode: HTMLElement | null = document.getElementById("mode")
 
-if(modeSwitch) modeSwitch.onclick = () => {
-  if (background && background.className === "light") {
-    background.className = "dark"
-    if(mode) mode.className = "darkmode"
-  } else {
-    if(background) background.className = "light"
-    if(mode) mode.className = "lightmode"
+if (modeSwitch)
+  modeSwitch.onclick = () => {
+    if (background && background.className === "light") {
+      background.className = "dark"
+      if (mode) mode.className = "darkmode"
+    } else {
+      if (background) background.className = "light"
+      if (mode) mode.className = "lightmode"
+    }
   }
-}
