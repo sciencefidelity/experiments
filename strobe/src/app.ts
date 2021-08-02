2228abc88908c79c3c2d262e270cfa65e73d86a3
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-02 01:05:34 +0000

    Fix linting warnings

--- strobe/src/app.ts
+++ strobe/src/app.ts
@@ -2,17 +2,17 @@
 
 "use strict"
 
-const shape: HTMLElement = document.getElementById("shape")
+const shape: HTMLElement | null = document.getElementById("shape")
 
-let i: number = 0
-shape.className = "c" + i
+let i = 0
+if (shape) shape.className = "c" + i
 
 const colorChange = () => {
   i++
   if (i === 6) {
     i = 0
   }
-  shape.className = "c" + i
+  if (shape) shape.className = "c" + i
 }
 
 setInterval(colorChange, 2000)
