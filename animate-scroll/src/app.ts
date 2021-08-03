d852d1d52fbffa8d51c19d90619e1e9da1771807
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-03 18:50:35 +0000

    Make colored container for the animation

--- animate-scroll/src/app.ts
+++ animate-scroll/src/app.ts
@@ -3,8 +3,19 @@ import { ScrollTrigger } from "gsap/ScrollTrigger"
 
 gsap.registerPlugin(ScrollTrigger)
 
-const tl = gsap.timeline({ scrollTrigger: "#box" })
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#container",
+    // start: "top 40%",
+    // end: "bottom 20%",
+    scrub: true,
+    pin: true,
+    markers: true,
+    toggleActions: "restart pause reverse pause"
+  }
+})
 
 // prettier-ignore
-tl.from("#box", { duration: 1.5, x: 270 })
-  .from("#box", { duration: 0.75, scale: 0.75 })
+tl.from("#box", { duration: 1, x: "75vw" })
+  .from("#box", { duration: 1, scale: 0.75 })
+  .from("#box", { duration: 1 })
