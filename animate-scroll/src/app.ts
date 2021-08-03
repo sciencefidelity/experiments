92efce4517c7d35d4fb46d3b127d0567ed47eae9
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-03 21:56:45 +0000

    Add new section

--- animate-scroll/src/app.ts
+++ animate-scroll/src/app.ts
@@ -5,17 +5,14 @@ gsap.registerPlugin(ScrollTrigger)
 
 const tl = gsap.timeline({
   scrollTrigger: {
-    trigger: "#container",
-    // start: "top 40%",
-    // end: "bottom 20%",
+    trigger: ".container",
+    end: "+=300%",
     scrub: true,
     pin: true,
-    markers: true,
-    toggleActions: "restart pause reverse pause"
   }
 })
 
 // prettier-ignore
-tl.from("#box", { duration: 1, x: "75vw" })
-  .from("#box", { duration: 1, scale: 0.75 })
-  .from("#box", { duration: 1 })
+tl.from(".box", { duration: 1, x: "80vw" })
+  .from(".box", { duration: 1, scale: 0.75 })
+  .from(".box", { duration: 0.5 })
