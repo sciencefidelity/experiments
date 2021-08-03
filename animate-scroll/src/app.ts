ecb80937fc5bd1e1e86890e50b1e772e7a68a0be
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-03 22:58:15 +0000

    Create horizontal scrolling section

--- animate-scroll/src/app.ts
+++ animate-scroll/src/app.ts
@@ -1,18 +1,35 @@
 import { gsap } from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
 
+const horizontalImages = document.querySelector(".container--b") as HTMLElement
+
 gsap.registerPlugin(ScrollTrigger)
 
 const tl = gsap.timeline({
   scrollTrigger: {
-    trigger: ".container",
+    trigger: ".container--a",
     end: "+=300%",
     scrub: true,
-    pin: true,
+    pin: true
   }
 })
 
 // prettier-ignore
-tl.from(".box", { duration: 1, x: "80vw" })
-  .from(".box", { duration: 1, scale: 0.75 })
-  .from(".box", { duration: 0.5 })
+tl.from(".title", { duration: 1, x: "80vw" })
+  .from(".title", { duration: 1, scale: 0.75 })
+  .from(".title", { duration: 0.5 })
+
+const sections = gsap.utils.toArray(".container__photo")
+
+// prettier-ignore
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".container--b",
+    pin: true,
+    scrub: 1,
+    snap: 1 / (sections.length - 1),
+    end: () => "+=" + horizontalImages.offsetWidth
+  }
+})
