36580c0e6fa0f3291eb809a23def6adfbc9a1b58
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-03 23:59:45 +0000

    Write regular expressions to match markdown

--- syntax/src/app.ts
+++ syntax/src/app.ts
@@ -11,26 +11,32 @@ input.addEventListener("input", updateValue)
 function updateValue(e: Event) {
   const target = e.target as HTMLDivElement
   localStorage.setItem("content", input.innerHTML)
+  // prettier-ignore
   if (target.textContent)
     output.innerHTML = target.textContent
-      //eslint-disable-next-line quotes
-      .replaceAll("Matt", '<span style="color:orange">Matt</span>')
-      //eslint-disable-next-line quotes
-      .replaceAll("World", '<span style="color:limegreen">World</span>')
+      .replaceAll("Matt", "<span style=\"color:orange\">Matt</span>")
+      .replaceAll("World", "<span style=\"color:limegreen\">World</span>")
 }
 
 // copy input text into output
+// prettier-ignore
 if (loadContent) {
   input.innerHTML = loadContent
-    //eslint-disable-next-line quotes
-    .replaceAll("Matt", '<span style="color:orange">Matt</span>')
-    //eslint-disable-next-line quotes
-    .replaceAll("World", '<span style="color:limegreen">World</span>')
+    .replaceAll("Matt", "<span style=\"color:orange\">Matt</span>")
+    .replaceAll("World", "<span style=\"color:limegreen\">World</span>")
 }
+// prettier-ignore
 if (loadContent) {
   output.innerHTML = loadContent
-    //eslint-disable-next-line quotes
-    .replaceAll("Matt", '<span style="color:orange">Matt</span>')
-    //eslint-disable-next-line quotes
-    .replaceAll("World", '<span style="color:limegreen">World</span>')
+    .replaceAll("Matt", "<span style=\"color:orange\">Matt</span>")
+    .replaceAll("World", "<span style=\"color:limegreen\">World</span>")
 }
+
+// Bold (double asterisk)
+// /(?:\*\*(?!\s).+?(?!\s)\*\*)/g
+
+// Heading
+// /^\#\s.+$/gm
+
+// Heading 2
+// /^\#\#\s.+$/gm
