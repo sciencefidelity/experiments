a61628c6c6bd820afd7263784279420389fbf8f7
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-09-27 12:39:29 +0100

    refactor: remove old versions

--- image-loader/README.md
+++ image-loader/README.md
@@ -6,4 +6,3 @@ Experiment to use pixelated placeholder images while larder images are loading.
 
 - Make the placeholder image tansition to the main image when it has loaded
 - Implement lazhy loading using the same technique for images lower in the page
- 
--- image-loader/src/app.ts
+++ image-loader/src/app.ts
@@ -1,15 +1,15 @@
 // Page is loaded
 const objects = document.querySelectorAll(
-  ".asyncImage"
+  ".async-image"
 ) as NodeListOf<HTMLImageElement>
 Array.from(objects).map(item => {
   // Start loading image
-  const img = new Image() as HTMLImageElement
+  const img = new Image()
   img.src = item.dataset.src || ""
   // Once image is loaded replace the src of the HTML element
   img.onload = () => {
-    item.classList.remove("asyncImage")
-    item.classList.add("fullImage")
+    item.classList.remove("async-image")
+    item.classList.add("full-image")
     return item.nodeName === "IMG"
       ? (item.src = item.dataset.src || "")
       : (item.style.backgroundImage = `url(${item.dataset.src})`)
