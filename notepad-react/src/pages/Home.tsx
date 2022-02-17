0a590bfc5d9c4bba02486e002901069b26d12f8e
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-02-17 19:15:06 +0000

    refactor: set up word and character counting and fix typescript warnings

--- notepad-react/src/pages/Home.tsx
+++ notepad-react/src/pages/Home.tsx
@@ -1,23 +1,26 @@
-import React, { useState } from "react"
+import React, { useState, FormEvent } from "react"
 
 const Experiment = () => {
   const [text, setText] = useState("")
   const [characterCount, setCharacterCount] = useState(0)
-  console.log(text.length)
+  const [wordCount, setWordCount] = useState(0)
+  const calculate = (e: FormEvent<HTMLDivElement>) => {
+    const content = (e.target as HTMLDivElement).textContent || ""
+    setText(content)
+    setCharacterCount(content.length)
+    setWordCount(content.split(" ").filter(e => e !== "").length)
+  }
   return (
     <>
       <div
         contentEditable="true"
         className="textbox"
-        onInput={e => {
-          setText(e.target.textContent)
-          setCharacterCount(text.length)
-        }}
+        onInput={calculate}
       />
       <div className="textbox">{text}</div>
       <div className="grid">
         <div className="inline">Characters: {characterCount}</div>
-        <div className="inline">Words: {text}</div>
+        <div className="inline">Words: {wordCount}</div>
       </div>
     </>
   )
