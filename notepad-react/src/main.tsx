572e04d3310d2f71c98d35b0cb8742642212398d
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-02-17 05:42:54 +0000

    feat: rename project directory, remove boilerplate, start to implemetent counter

--- notepad-react/src/main.tsx
+++ notepad-react/src/main.tsx
@@ -1,26 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { Router } from "@reach/router"
 import "sanitize.css/sanitize.css"
 import "sanitize.css/assets.css"
 import "sanitize.css/reduce-motion.css"
 import "sanitize.css/typography.css"
 import "styles/globals.scss"
-import About from "pages/About"
-import Counter from "pages/Counter"
-import Experiment from "pages/Experiment"
 import Home from "pages/Home"
-import NotFound from "pages/404"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Home path="/" title="home" />
-      <Counter path="/counter" title="counter" />
-      <About path="/about" title="about" />
-      <Experiment path="/experiment" title="experiment" />
-      <NotFound default />
-    </Router>
+    <Home path="/" title="home" />
   </React.StrictMode>,
   document.getElementById("root")
 )
