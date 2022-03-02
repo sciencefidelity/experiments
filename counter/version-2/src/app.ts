2face14acbe03d20b96a85f97900e714955910ca
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-03-02 21:50:27 +0000

    feat: combine a counter with an intersection observer

---
+++ counter/version-2/src/app.ts
@@ -0,0 +1,38 @@
+const counterContainer = document.querySelector(".counter") as HTMLDivElement
+const counter = document.querySelector(".counter") as HTMLDivElement
+const speed = 205
+const options = {
+  rootMargin: "-80px"
+}
+
+let timerId: NodeJS.Timeout
+
+const observer = new IntersectionObserver(entries => {
+  clearTimeout(timerId)
+  counter.innerText = "0"
+  if (!entries[0].isIntersecting) {
+    return
+  }
+  counting()
+}, options)
+
+observer.observe(counterContainer)
+
+const counting = () => {
+  let count = 0
+  counter.innerText = count.toString()
+  const updateCount = () => {
+    const target = parseInt(counter.getAttribute("data-target") ?? "0")
+    const inc = target / speed
+    if (count < target) {
+      count = count + inc
+      counter.innerText = Math.floor(count).toString()
+      timerId = setTimeout(updateCount, 100)
+    } else {
+      counter.innerText = target.toString()
+      clearTimeout(timerId)
+    }
+    console.log("counted")
+  }
+  updateCount()
+}
