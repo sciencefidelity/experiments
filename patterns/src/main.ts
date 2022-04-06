db13627c9e659f34933a0e38a06301b245cae971
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-04-06 21:45:40 -0200

    feat: setup new project

---
+++ patterns/src/main.ts
@@ -0,0 +1,8 @@
+import "sanitize.css/sanitize.css"
+import "sanitize.css/assets.css"
+import "sanitize.css/reduce-motion.css"
+import "sanitize.css/typography.css"
+import "@/style.scss"
+
+const canvas = document.querySelector("#canvas") as HTMLDivElement
+
