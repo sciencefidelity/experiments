4acc97a4d1a1168642a58ffe1c88ef6e38b7ea09
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-04-06 22:10:57 -0200

    feat: generate lines in a canvas

--- patterns/src/main.ts
+++ patterns/src/main.ts
@@ -6,3 +6,18 @@ import "@/style.scss"
 
 const canvas = document.querySelector("#canvas") as HTMLDivElement
 
+// const canvasHeight = 400
+// const lineGap = 40
+
+const initializeCanvas = () => {
+  for (let i = 1; i <= 9; i++) {
+    const line = document.createElement("div")
+    line.className = "line"
+    // line.style.width = "100%"
+    // line.style.height = "5px"
+    // line.style.backgroundColor = "hsl(0, 0%, 0%)"
+    // line.style.backgroundColor = "red"
+    canvas.appendChild(line)
+  }
+}
+initializeCanvas()
