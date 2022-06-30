73df13b4a24c38bafbf143f602a210506339c572
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-30 23:26:08 +0100

    refactor: get loading lazy to work

--- lazy/src/App.tsx
+++ lazy/src/App.tsx
@@ -6,37 +6,43 @@ const App = () => {
     setLoaded(true)
   }, [])
   useEffect(() => {
-    setLoaded(true)
+    let ignore = false
+    if (!ignore) {
+      setLoaded(true)
+    }
+    return () => {
+      ignore = true
+    }
   }, [loaded, onLoad])
   
-  const imageUrl = "https://picsum.photos/1200/800"
-  const fallbackUrl = "https://picsum.photos/12/8"
+  const imageUrl = "https://picsum.photos/600/800"
+  const fallbackUrl = "https://picsum.photos/6/8"
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  // const arr = [1]
   const alphabet = "abcdefghijklmnopqrstuvwxyz"
-  const handleOnLoad = () => {
-    console.log("Image loaded")
-    
-  }
+
   return (
     <div>
-      {arr.map(num => (
+      {arr.map((num, idx) => (
         <div
           key={num}
           className="box"
         >
           <img
+            src={`${fallbackUrl}?${alphabet[num]}`}
+            className="placeholder"
+            alt=""
+            loading={idx === 0 ? "eager" : "lazy"}
+            width={600}
+            height={800}
+          />
+          <img
             src={`${imageUrl}?${alphabet[num]}`}
             className={`image ${loaded ? "loaded" : null}`}
             alt=""
             loading="lazy"
-            onLoad={handleOnLoad}
-          />
-          <img
-            src={`${fallbackUrl}?${alphabet[num]}`}
-            className="placeholder"
-            alt=""
-            loading="eager"
-            width={1200}
+            onLoad={onLoad}
+            width={600}
             height={800}
           />
         </div>
