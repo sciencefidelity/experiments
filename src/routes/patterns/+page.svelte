0293c297fa3cd38065298393fdddbef9453fd168
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-09-28 17:50:32 +0100

    feat: migrate patterns and rainbow

---
+++ src/routes/patterns/+page.svelte
@@ -0,0 +1,328 @@
+<svelte:head>
+	<title>Patterns</title>
+</svelte:head>
+
+<div class="container">
+	<div id="dots" class="box"><button class="btn">Register</button></div>
+	<div id="circles" class="box"><button class="btn">Register</button></div>
+	<div id="lines" class="box"><button class="btn">Register</button></div>
+	<div id="lines-vertical" class="box"><button class="btn">Register</button></div>
+	<div id="squares" class="box"><button class="btn">Register</button></div>
+	<div id="cubes" class="box"><button class="btn">Register</button></div>
+	<div id="diagonal" class="box"><button class="btn">Register</button></div>
+	<div id="textbook" class="box"><button class="btn">Register</button></div>
+	<div id="cross" class="box"><button class="btn">Register</button></div>
+	<div id="isometric" class="box"><button class="btn">Register</button></div>
+	<div id="radial" class="box"><button class="btn">Register</button></div>
+	<div id="repeating-circles" class="box"><button class="btn">Register</button></div>
+	<div id="zigzag" class="box"><button class="btn">Register</button></div>
+	<div id="isometric-3d" class="box"><button class="btn">Register</button></div>
+	<div id="target" class="box"><button class="btn">Register</button></div>
+</div>
+
+<style lang="scss">
+	:root {
+		--foreground: hsl(0, 0%, 0%);
+		--pattern: hsl(0, 0%, 75%);
+		--pattern-light: hsla(0, 0%, 75%, 50%);
+		--background: hsl(60, 15%, 95%);
+	}
+
+	:global(body) {
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
+
+	.container {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 2rem;
+		justify-content: center;
+		margin: 2rem 0;
+	}
+
+	.box {
+		// margin: 2rem auto;
+		display: grid;
+		flex: 0 1 410px;
+		place-items: center;
+		width: 410px;
+		height: 410px;
+		background-color: var(--background);
+		background-repeat: repeat;
+		border: 1px solid var(--foreground);
+	}
+
+	#dots {
+		background-image: radial-gradient(var(--pattern) 12%, transparent 12%),
+			radial-gradient(var(--pattern) 12%, transparent 12%);
+		background-position:
+			0 0,
+			10px 10px;
+		background-size: 20px 20px;
+	}
+
+	#circles {
+		background-image: radial-gradient(var(--pattern) 9%, transparent 9%),
+			radial-gradient(var(--pattern) 9%, transparent 9%);
+		background-position: center;
+		// background-position: 0 0, 16px 16px;
+		background-size: 32px 32px;
+	}
+
+	#lines {
+		background-image: linear-gradient(
+			0deg,
+			transparent 49%,
+			var(--pattern) 49%,
+			var(--pattern) 50%,
+			transparent 50%,
+			transparent 99%,
+			var(--pattern) 99%,
+			var(--pattern) 100%
+		);
+		background-position: center;
+		background-size: 62px 62px;
+	}
+
+	#lines-vertical {
+		background-image: linear-gradient(
+			90deg,
+			transparent 49%,
+			var(--pattern) 49%,
+			var(--pattern) 50%,
+			transparent 50%,
+			transparent 99%,
+			var(--pattern) 99%,
+			var(--pattern) 100%
+		);
+		background-position: center;
+		background-size: 62px 62px;
+	}
+
+	#squares {
+		background-image: linear-gradient(
+				0deg,
+				transparent 49%,
+				var(--pattern) 49%,
+				var(--pattern) 50%,
+				transparent 50%,
+				transparent 99%,
+				var(--pattern) 99%,
+				var(--pattern) 100%
+			),
+			linear-gradient(
+				90deg,
+				transparent 49%,
+				var(--pattern) 49%,
+				var(--pattern) 50%,
+				transparent 50%,
+				transparent 99%,
+				var(--pattern) 99%,
+				var(--pattern) 100%
+			);
+		background-position: center;
+		background-size: 62px 62px;
+	}
+
+	#cubes {
+		background-image: linear-gradient(
+				0deg,
+				transparent 49.5%,
+				var(--pattern) 49.5%,
+				var(--pattern) 50%,
+				transparent 50%,
+				transparent 99.5%,
+				var(--pattern) 99.5%,
+				var(--pattern) 100%
+			),
+			linear-gradient(
+				90deg,
+				transparent 49.5%,
+				var(--pattern) 49.5%,
+				var(--pattern) 50%,
+				transparent 50%,
+				transparent 99.5%,
+				var(--pattern) 99.5%,
+				var(--pattern) 100%
+			);
+		background-position: center;
+		background-size: 120px 120px;
+	}
+
+	#diagonal {
+		background-image: linear-gradient(
+				45deg,
+				transparent 49%,
+				var(--pattern) 49%,
+				var(--pattern) 50%,
+				transparent 50%,
+				transparent 99%,
+				var(--pattern) 99%,
+				var(--pattern) 100%
+			),
+			linear-gradient(
+				-45deg,
+				transparent 49%,
+				var(--pattern) 49%,
+				var(--pattern) 50%,
+				transparent 50%,
+				transparent 99%,
+				var(--pattern) 99%,
+				var(--pattern) 100%
+			);
+		background-position: center;
+		background-size: 21px 21px;
+	}
+
+	#textbook {
+		background-image: linear-gradient(var(--pattern) 2px, transparent 2px),
+			linear-gradient(90deg, var(--pattern) 2px, transparent 2px),
+			linear-gradient(var(--pattern) 1px, transparent 1px),
+			linear-gradient(90deg, var(--pattern) 1px, transparent 1px);
+		background-position: center;
+		background-size:
+			100px 100px,
+			100px 100px,
+			20px 20px,
+			20px 20px;
+		opacity: 0.8;
+	}
+
+	#target {
+		background-image: repeating-radial-gradient(var(--pattern), 1px, transparent 30px);
+		// opacity: 0.8;
+	}
+
+	#repeating-circles {
+		background-image: repeating-radial-gradient(var(--pattern) 0 0.2%, transparent 0.2% 2%);
+		// background-position: center;
+		// background-size: 410px 410px;
+	}
+
+	#radial {
+		background-image: radial-gradient(transparent 21%, var(--pattern) 21% 24%, transparent 26%);
+		background-position: center;
+		background-size: 21px 21px;
+	}
+
+	#zigzag {
+		background-image: linear-gradient(135deg, var(--pattern) 25%, transparent 25%),
+			linear-gradient(225deg, var(--pattern) 25%, transparent 25%),
+			linear-gradient(45deg, var(--pattern) 25%, transparent 25%),
+			linear-gradient(315deg, var(--pattern) 25%, transparent 25%);
+		background-position:
+			20px 0,
+			20px 0,
+			0 0,
+			0 0;
+		background-size: 40px 80px;
+		// opacity: 0.8;
+	}
+
+	#isometric {
+		background-image: url('/isometric.svg');
+		background-size: 60px;
+		background-position: center;
+	}
+
+	#isometric-3d {
+		background-image: linear-gradient(
+				30deg,
+				var(--pattern) 12%,
+				transparent 12.5%,
+				transparent 87%,
+				var(--pattern) 87.5%,
+				var(--pattern)
+			),
+			linear-gradient(
+				150deg,
+				var(--pattern) 12%,
+				transparent 12.5%,
+				transparent 87%,
+				var(--pattern) 87.5%,
+				var(--pattern)
+			),
+			linear-gradient(
+				30deg,
+				var(--pattern) 12%,
+				transparent 12.5%,
+				transparent 87%,
+				var(--pattern) 87.5%,
+				var(--pattern)
+			),
+			linear-gradient(
+				150deg,
+				var(--pattern) 12%,
+				transparent 12.5%,
+				transparent 87%,
+				var(--pattern) 87.5%,
+				var(--pattern)
+			),
+			linear-gradient(
+				60deg,
+				var(--pattern-light) 25%,
+				transparent 25.5%,
+				transparent 75%,
+				var(--pattern-light) 75%,
+				var(--pattern-light)
+			),
+			linear-gradient(
+				60deg,
+				var(--pattern-light) 25%,
+				transparent 25.5%,
+				transparent 75%,
+				var(--pattern-light) 75%,
+				var(--pattern-light)
+			);
+		background-position:
+			0 0,
+			0 0,
+			10px 18px,
+			10px 18px,
+			0 0,
+			10px 18px;
+		background-size: 20px 35px;
+		// opacity: 0.8;
+	}
+
+	#cross {
+		background:
+			radial-gradient(
+				circle,
+				transparent 20%,
+				var(--background) 20%,
+				var(--background) 80%,
+				transparent 80%,
+				transparent
+			),
+			radial-gradient(
+					circle,
+					transparent 20%,
+					var(--background) 20%,
+					var(--background) 80%,
+					transparent 80%,
+					transparent
+				)
+				25px 25px,
+			linear-gradient(var(--pattern) 1px, transparent 1px) 0 -1px,
+			linear-gradient(90deg, var(--pattern) 1px, var(--background) 1px) -1px 0;
+		background-size:
+			50px 50px,
+			50px 50px,
+			25px 25px,
+			25px 25px;
+		// opacity: 0.8;
+	}
+
+	.btn {
+		width: 150px;
+		height: 50px;
+		font-size: 1.5rem;
+		// text-transform: lowercase;
+		cursor: pointer;
+		background: hsl(0, 100%, 100%);
+		border: 2px solid var(--foreground);
+		border-radius: 999px;
+	}
+</style>
